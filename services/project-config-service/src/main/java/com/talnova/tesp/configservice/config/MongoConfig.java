9230de97f7b95852f66059f66f0b44af6a7b18d6
kavi castelo <kavindu123450@gmail.com>
2026-08-07 00:41:56 +0530

    dockerization fixing attempt

--- services/project-config-service/src/main/java/com/talnova/tesp/configservice/config/MongoConfig.java
+++ services/project-config-service/src/main/java/com/talnova/tesp/configservice/config/MongoConfig.java
@@ -2,8 +2,10 @@ package com.talnova.tesp.configservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoAuditing
+@EnableMongoRepositories(basePackages = "com.talnova.tesp.configservice.repository")
 public class MongoConfig {
 }
